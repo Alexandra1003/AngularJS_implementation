35c31da94c751148d65e712080bfd9d5c0490ea6
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-02-12 11:27:32 +0200

    master, initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AngularJS_implementation
+Create partial implementation of AngularJS
---
+++ src/index.js
@@ -0,0 +1,16 @@
+(function() {
+  const directives = [{}];
+
+  const smallAngular = {
+    directive() {
+      // do smth
+    },
+    compile(node) {
+      // do smth
+    },
+    bootstrap(node) {
+      // do smth
+    }
+  };
+  window.smallAngular = smallAngular;
+})();
